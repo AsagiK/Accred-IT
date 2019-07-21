6a0d175b40dd7920a44dbd5f506ce941f209736c
roano <43611468+roano@users.noreply.github.com>
2019-07-21 20:33:59 +0800

    Create Dump20190721.sql

---
+++ gentelella-master/database/Dump20190721.sql
@@ -0,0 +1,364 @@
+CREATE DATABASE  IF NOT EXISTS `capstone` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `capstone`;
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: capstone
+-- ------------------------------------------------------
+-- Server version	8.0.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accreditation`
+--
+
+DROP TABLE IF EXISTS `accreditation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `accreditation` (
+  `accreditation_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `accreditation_Name` varchar(200) DEFAULT NULL,
+  `accreditation_Description` mediumtext,
+  PRIMARY KEY (`accreditation_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accreditation`
+--
+
+LOCK TABLES `accreditation` WRITE;
+/*!40000 ALTER TABLE `accreditation` DISABLE KEYS */;
+INSERT INTO `accreditation` VALUES (1,'PAASCU','PAASCU is governed by a 15-person Board of Directors elected at large by\r\nmembers during the annual General Assembly. It has seven Commissions with five\r\nto seven members each to perform its mandate. These are the Commissions on\r\nGraduate Education, Medical Education, Engineering Education, Tertiary\r\nEducation, Integrated Basic Education, Secondary Education and Elementary\r\nEducation. The Commission members are recommended during the General\r\nAssembly and appointed by the Board.\r\n\r\nThe Commissions plan and initiate projects for each level, revise survey\r\ninstruments, and train accreditors and team chairs. Moreover, they review the\r\nreports of the survey teams before these are submitted to the Board.\r\nPAASCU’s day to day operations are handled by a Secretariat which is headed by\r\nthe Executive Director. The Secretariat takes care of the logistics of the survey\r\nvisits, invites accreditors, prepares reports and forms, and implements the projects\r\nof the Commissions.'),(2,'ABET','At ABET, our purpose is to assure confidence in university programs in STEM (science, technology, engineering and mathematics) disciplines. Our approach, the standards we set and the quality we guarantee, inspires confidence in those who aim to build a better world—one that is safer, more efficient, more comfortable and more sustainable.\r\n\r\nWe accredit college and university programs in the disciplines of applied and natural science, computing, engineering and engineering technology at the associate, bachelor’s and master’s degree levels.\r\n\r\nWith ABET accreditation, students, employers and the society we serve can be confident that a program meets the quality standards that produce graduates prepared to enter a global workforce.\r\n\r\nWe began as the educational standard against which professional engineers in the United States were held for licensure. Today, after more than 80 years, our standards continue to play this fundamental role and have become the basis of quality for STEM disciplines all over the world.\r\n\r\nDeveloped by technical professionals from ABET’s member societies, our criteria focus on what students experience and learn. Sought worldwide, ABET’s voluntary peer-review process is highly respected because it adds critical value to academic programs in the technical disciplines, where quality, precision and safety are of the utmost importance.\r\n\r\nOur more than 2,200 experts come from industry, academia and government. They give their time and effort supporting quality assurance activities around the world by serving as Program Evaluators, commissioners, board members and advisors.\r\n\r\nABET is a nonprofit, non-governmental organization with ISO 9001:2015 certification.');
+/*!40000 ALTER TABLE `accreditation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `area`
+--
+
+DROP TABLE IF EXISTS `area`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `area` (
+  `Area_ID` int(11) NOT NULL,
+  `Area_Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Area_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `area`
+--
+
+LOCK TABLES `area` WRITE;
+/*!40000 ALTER TABLE `area` DISABLE KEYS */;
+INSERT INTO `area` VALUES (1,'Purposes and Objectives'),(2,'Faculty'),(3,'Instruction'),(4,'Library'),(5,'Laboratories'),(6,'Physial Plan'),(7,'Student Services'),(8,'Administration'),(9,'School and Community');
+/*!40000 ALTER TABLE `area` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cycle`
+--
+
+DROP TABLE IF EXISTS `cycle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cycle` (
+  `cycle_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `cycle_Name` varchar(45) NOT NULL,
+  `start_Date` date NOT NULL,
+  `status` varchar(45) NOT NULL DEFAULT 'Planning',
+  PRIMARY KEY (`cycle_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cycle`
+--
+
+LOCK TABLES `cycle` WRITE;
+/*!40000 ALTER TABLE `cycle` DISABLE KEYS */;
+INSERT INTO `cycle` VALUES (0,'No Assigned ','0000-00-00','Planning');
+/*!40000 ALTER TABLE `cycle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `documents` (
+  `Document_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Document_Name` varchar(45) NOT NULL,
+  `Document_Route` varchar(100) NOT NULL,
+  `Document_Desc` varchar(300) DEFAULT 'No Description Given.',
+  `Document_Ext` varchar(45) NOT NULL,
+  PRIMARY KEY (`Document_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `documents`
+--
+
+LOCK TABLES `documents` WRITE;
+/*!40000 ALTER TABLE `documents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `documents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `group`
+--
+
+DROP TABLE IF EXISTS `group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `group` (
+  `Group_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Group_Name` varchar(45) DEFAULT NULL,
+  `Area_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Group_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group`
+--
+
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+INSERT INTO `group` VALUES (0,'No Group Assigned',0),(1,'Analysis',1),(2,'Evaluation',1),(3,'Analysis',2),(4,'Evaluation',2),(5,'Analysis',3),(6,'Evaluation',3),(7,'Analysis',4),(8,'Evaluation',4),(9,'Analysis',5),(10,'Evaluation',5),(11,'Analysis',6),(12,'Evaluation',6),(13,'Analysis',7),(14,'Evaluation',7),(15,'Analysis',8),(16,'Evaluation',8),(17,'Analysis',9),(18,'Evaluation',9),(19,'Test Area',1),(20,'TESTING',2);
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groupdetails`
+--
+
+DROP TABLE IF EXISTS `groupdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `groupdetails` (
+  `Groupdetails_ID` int(11) NOT NULL,
+  `Groupdetails_UserID` int(11) NOT NULL,
+  `Groupdetails_Position` varchar(45) NOT NULL DEFAULT 'Member'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groupdetails`
+--
+
+LOCK TABLES `groupdetails` WRITE;
+/*!40000 ALTER TABLE `groupdetails` DISABLE KEYS */;
+INSERT INTO `groupdetails` VALUES (1,29,'Member'),(1,30,'Member'),(1,31,'Member'),(2,32,'Member'),(1,35,'Member'),(1,40,'Member'),(1,41,'Member'),(1,36,'Member'),(1,38,'Member'),(1,37,'Member'),(1,39,'Member');
+/*!40000 ALTER TABLE `groupdetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plans`
+--
+
+DROP TABLE IF EXISTS `plans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `plans` (
+  `Plan_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `GenObjective` varchar(200) NOT NULL,
+  `Measurement` varchar(200) NOT NULL,
+  `BaseFormula` varchar(200) NOT NULL,
+  `QualityTarget` varchar(200) NOT NULL,
+  `Procedures` varchar(500) NOT NULL,
+  `GroupAssigned` int(11) DEFAULT NULL,
+  `CycleTime` int(11) DEFAULT '0',
+  `PriorityLevel` varchar(45) DEFAULT 'No Priority Level Set',
+  `BaseStandard` varchar(200) DEFAULT 'No Base Standard Set',
+  `recommendation_ID` int(11) DEFAULT NULL,
+  `PlanName` varchar(45) NOT NULL,
+  `PlanDescription` varchar(200) DEFAULT NULL,
+  `Plan_MinCycles` int(11) DEFAULT '3',
+  `Deadline` varchar(45) DEFAULT NULL,
+  `CycleCount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Plan_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plans`
+--
+
+LOCK TABLES `plans` WRITE;
+/*!40000 ALTER TABLE `plans` DISABLE KEYS */;
+INSERT INTO `plans` VALUES (4,'234','234','234','2342','afhjlaekgjhaeblrkjgberlgqhtjherlguiherwqiughbjakbnvkjsldhailufaheaiurthqiuotyeiuqrheijkahlagkjhvhkajdbvauerigfoiuqeyrogtiuqeghfkjahlgakjrhgaiuereyhgiuqlrhejikfnevkjrenaufiahrgiuahvkiajhvkzjhdfvkjgdfjkhagukeygqiruytqiuegfuiasdhlvjadskvlhkdfvhzjkbdvzknmdfbga,kjgrhakejhwktiuewyrhtriouhvzlakjvbjkzhfdhgakjfghaueiryquierklhgajbvkjsfdbvsd',1,0,'High','No base standard assigned',15,'234','234',3,NULL,NULL);
+/*!40000 ALTER TABLE `plans` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `players` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `number` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recommendation`
+--
+
+DROP TABLE IF EXISTS `recommendation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `recommendation` (
+  `recommendation_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `recommendation_Name` varchar(45) DEFAULT NULL,
+  `recommendation_Desc` varchar(200) DEFAULT NULL,
+  `recommendation_Grade` varchar(45) DEFAULT NULL,
+  `priority_Level` varchar(45) DEFAULT NULL,
+  `date_insert` datetime DEFAULT NULL,
+  `area_ID` int(11) DEFAULT NULL,
+  `group_ID` int(11) DEFAULT '0',
+  `accreditation_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`recommendation_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recommendation`
+--
+
+LOCK TABLES `recommendation` WRITE;
+/*!40000 ALTER TABLE `recommendation` DISABLE KEYS */;
+INSERT INTO `recommendation` VALUES (15,'123','123','F','Low','2019-07-14 00:00:00',1,1,1);
+/*!40000 ALTER TABLE `recommendation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `roles` (
+  `Role_ID` int(11) NOT NULL,
+  `Role_Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Role_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Admin'),(2,'Group Member'),(3,'Group Leader'),(4,'QA Officer'),(5,'User');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tasks` (
+  `task_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Task_Name` varchar(45) NOT NULL,
+  `Task_Desc` varchar(45) NOT NULL,
+  `GenObj` varchar(45) NOT NULL,
+  `Measurement` varchar(45) NOT NULL,
+  `QT` varchar(45) NOT NULL,
+  `BaseStandard` varchar(45) NOT NULL,
+  `Group` varchar(45) DEFAULT 'Not Assigned',
+  `Personnel` varchar(45) DEFAULT 'Not Assigned',
+  `Level` varchar(45) DEFAULT 'Not Assigned',
+  `plan_ID` int(11) NOT NULL,
+  PRIMARY KEY (`task_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `User_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `User_First` varchar(45) DEFAULT NULL,
+  `User_Last` varchar(45) DEFAULT NULL,
+  `email_address` varchar(45) DEFAULT NULL,
+  `Role` int(11) DEFAULT NULL,
+  `Group` int(11) DEFAULT NULL,
+  `ContactNo` varchar(12) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `passwd` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`User_ID`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (35,'Admin','Admin','debug@debug.com',1,1,'99999999999','Admin','$2b$10$w1q.GZyvVMwt9c6aiFjBEuahRim1NOtIgFOY8DYsxoJuN31CsUGsu'),(36,'test','test','test@test.com',5,1,'091827365','testtest2',NULL),(37,'test2','test2','test2@test.com',5,1,'1239612874','test2test22',NULL),(38,'test3','test3','test3@test.com',5,1,'234142423','test3test33',NULL),(39,'test4','test4','test4@test.com',5,1,'81926815','test4test44',NULL),(40,'test5','test5','test5@test.com',5,1,'192863124','test5test55',NULL),(41,'test6','test6','test6@test.com',5,1,'21314124214','test6test66',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-07-21 20:32:58
