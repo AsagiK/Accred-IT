06cf37ad367491adc67ba28555012906460f9c46
AsagiK <42568800+AsagiK@users.noreply.github.com>
2020-03-09 11:33:03 +0800

    pls

--- gentelella-master/controllers/AdminController.js
+++ gentelella-master/controllers/AdminController.js
@@ -5,27 +5,47 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const md5 = require('md5');
+const async = require("async");
 var mysql = require('mysql');
 var connection = require('../config/db');
+try {
+    var TOKEN_PATH = './config/token.json'
+    var UPLOAD_PATH = JSON.parse(fs.readFileSync('./config/accredit.json', 'utf8'));
+} catch (e) {
+
+}
+const disk = require('diskusage');
+const os = require('os');
+const mysqldump = require('mysqldump')
+
+
 // ---- URL PARSER
 var url = require('url');
 var session = require('express-session');
 // ---- DEFINE SESSION
 server.use(session({
-    secret: 'ssshhhhh'
+    secret: 'ssshhhhh',
+    resave: false,
+    saveUninitialized: true
 }));
-// ----
-var sess;
+// ---- GOOGLE API
+const readline = require('readline');
+const {
+    google
+} = require('googleapis');
+
+server.use(fileUpload({
+    createParentPath: true,
+    safeFileNames: true,
+    preserveExtension: 10
+}));
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+var mime = require('mime-types');
+var Notif = require('../controllers/NotifController')
+var sess
+
+
 module.exports = {
 
-    sess = req.session;
-    if (!req.session.user) {
-        console.log("No session")
-        resp.redirect('/login?status=0');
-    } else {
-        {
-            current_user: sess.user
-        }
-    }
 
 }
