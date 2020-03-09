91a1ce27a58ceeeaf887439686716fc23fa0d54e
AsagiK <42568800+AsagiK@users.noreply.github.com>
2020-03-09 11:15:50 +0800

    admincontroller

---
+++ gentelella-master/controllers/AdminController.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const server = express();
+const fs = require('fs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+var mysql = require('mysql');
+var connection = require('../config/db');
+// ---- URL PARSER
+var url = require('url');
+var session = require('express-session');
+// ---- DEFINE SESSION
+server.use(session({
+    secret: 'ssshhhhh'
+}));
+// ----
+var sess;
+module.exports = {
+
+    sess = req.session;
+    if (!req.session.user) {
+        console.log("No session")
+        resp.redirect('/login?status=0');
+    } else {
+        {
+            current_user: sess.user
+        }
+    }
+
+}
